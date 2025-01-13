443d040a6291823a64bc7954de288ad71898843f
moon <moon@vessl.ai>
2025-01-13 10:37:45 +0900

    feat: run wheel instead

--- libs/hyperpocket/hyperpocket/server/tool/dto/script.py
+++ libs/hyperpocket/hyperpocket/server/tool/dto/script.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, Field
 
 from hyperpocket.tool.wasm.script import ScriptFileNode
@@ -8,8 +9,20 @@ class Script(BaseModel):
     tool_id: str = Field(alias='tool_id')
 
 
-class ScriptStdout(BaseModel):
-    stdout: str = Field(alias='stdout')
+class ScriptResult(BaseModel):
+    stdout: Optional[str] = Field(alias='stdout', default=None)
+    stderr: Optional[str] = Field(alias='stderr', default=None)
+    error: Optional[str] = Field(alias='error', default=None)
 
 class ScriptFileTree(BaseModel):
     tree: dict[str, ScriptFileNode] = Field(alias='tree')
+
+class ScriptEntrypoint(BaseModel):
+    package_name: Optional[str] = Field(alias='package_name')
+    entrypoint: str = Field(alias='entrypoint')
+
+class ScriptEncodedFile(BaseModel):
+    encoded_file: str = Field(alias='encoded_file')
+
+class ScriptFileRequest(BaseModel):
+    path: str = Field(alias='path')
