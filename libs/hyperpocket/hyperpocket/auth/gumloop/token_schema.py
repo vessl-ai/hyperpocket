7b78033e99db99e33d707d51d3efe267f253fd21
moon <moon@vessl.ai>
2025-01-16 15:51:19 +0900

    add gumloop token auth

---
+++ libs/hyperpocket/hyperpocket/auth/gumloop/token_schema.py
@@ -0,0 +1,8 @@
+from hyperpocket.auth.schema import AuthenticateRequest, AuthenticateResponse
+
+
+class GumloopTokenRequest(AuthenticateRequest):
+    pass
+
+class GumloopTokenResponse(AuthenticateResponse):
+    access_token: str
