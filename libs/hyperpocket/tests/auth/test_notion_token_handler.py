2dcbead7d47b436e3539914d5eb7f75136e3875c
nsd9696 <nsd26534613@gmail.com>
2025-01-16 13:30:10 +0900

    give unique state, redirect_url etc per auth

--- libs/hyperpocket/tests/auth/test_notion_token_handler.py
+++ libs/hyperpocket/tests/auth/test_notion_token_handler.py
@@ -16,8 +16,8 @@ class TestNotionTokenAuthHandler(IsolatedAsyncioTestCase):
     async def test_make_auth_url(self):
         auth_url = self.handler._make_auth_url(
             auth_req=self.auth_req,
-            redirect_uri="http://test-redirect-uri.com",
-            state="test-future-id"
+            redirect_uri="http://test-notion-redirect-uri.com",
+            state="test-notion-future-id"
         )
         parsed = urlparse(auth_url)
         query_params = parse_qs(parsed.query)
@@ -25,42 +25,42 @@ class TestNotionTokenAuthHandler(IsolatedAsyncioTestCase):
 
         # then
         self.assertEqual(base_url, self.handler._TOKEN_URL)
-        self.assertEqual(query_params["state"][0], "test-future-id")
-        self.assertEqual(query_params["redirect_uri"][0], "http://test-redirect-uri.com")
+        self.assertEqual(query_params["state"][0], "test-notion-future-id")
+        self.assertEqual(query_params["redirect_uri"][0], "http://test-notion-redirect-uri.com")
 
     async def test_prepare(self):
         # when
         prepare: str = self.handler.prepare(
             auth_req=self.auth_req,
-            thread_id="test-prepare-thread-id",
-            profile="test-prepare-profile",
-            future_uid="test-prepare-future-uid",
+            thread_id="test-notion-prepare-thread-id",
+            profile="test-notion-prepare-profile",
+            future_uid="test-notion-prepare-future-uid",
         )
         auth_url = prepare.removeprefix("User needs to authenticate using the following URL:").strip()
-        future_data = FutureStore.get_future( uid="test-prepare-future-uid")
+        future_data = FutureStore.get_future( uid="test-notion-prepare-future-uid")
 
         # then
         self.assertTrue(auth_url.startswith(self.handler._TOKEN_URL))
         self.assertIsNotNone(future_data)
-        self.assertEqual(future_data.data["thread_id"], "test-prepare-thread-id")
-        self.assertEqual(future_data.data["profile"], "test-prepare-profile")
+        self.assertEqual(future_data.data["thread_id"], "test-notion-prepare-thread-id")
+        self.assertEqual(future_data.data["profile"], "test-notion-prepare-profile")
         self.assertFalse(future_data.future.done())
 
     async def test_authenticate(self):
         self.handler.prepare(
             auth_req=self.auth_req,
-            thread_id="test-thread-id",
-            profile="test-profile",
-            future_uid="test-future-uid"
+            thread_id="test-notion-thread-id",
+            profile="test-notion-profile",
+            future_uid="test-notion-future-uid"
         )
-        future_data = FutureStore.get_future( uid="test-future-uid")
-        future_data.future.set_result("test-token")
+        future_data = FutureStore.get_future( uid="test-notion-future-uid")
+        future_data.future.set_result("test-notion-token")
 
         response: NotionTokenAuthContext = await self.handler.authenticate(
             auth_req=self.auth_req,
-            future_uid="test-future-uid"
+            future_uid="test-notion-future-uid"
         )
 
         self.assertIsInstance(response, NotionTokenAuthContext)
-        self.assertEqual(response.access_token, "test-token")
+        self.assertEqual(response.access_token, "test-notion-token")
         self.assertIsNone(response.expires_at)
