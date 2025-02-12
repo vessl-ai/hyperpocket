973aff21900d960841c6f5d8b26ae4dcaf697079
Jaeman An <ajmbell@gmail.com>
2025-02-12 12:18:56 -0800

    add render

---
+++ examples/dynamic-tool-calling/web/backend/main.py
@@ -0,0 +1,10 @@
+from fastapi.middleware.cors import CORSMiddleware
+import os
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=os.getenv("CORS_ORIGINS", "").split(","),
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+) 
