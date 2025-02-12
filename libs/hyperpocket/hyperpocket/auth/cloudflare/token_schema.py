20a046805f1423ece8656daf7eb2c2f5243ee826
nsd9696 <nsd26534613@gmail.com>
2025-02-11 23:50:29 -0800

    add more token auth

---
+++ libs/hyperpocket/hyperpocket/auth/cloudflare/token_schema.py
@@ -0,0 +1,8 @@
+
+from typing import List, Optional
+from pydantic import BaseModel
+from hyperpocket.auth.schema import AuthenticateRequest, AuthenticateResponse
+class CloudflareTokenRequest(AuthenticateRequest):
+    pass
+class CloudflareTokenResponse(AuthenticateResponse):
+    access_token: str
---
+++ libs/hyperpocket/hyperpocket/auth/timekit/token_schema.py
@@ -0,0 +1,8 @@
+
+from typing import List, Optional
+from pydantic import BaseModel
+from hyperpocket.auth.schema import AuthenticateRequest, AuthenticateResponse
+class TimekitTokenRequest(AuthenticateRequest):
+    pass
+class TimekitTokenResponse(AuthenticateResponse):
+    access_token: str
