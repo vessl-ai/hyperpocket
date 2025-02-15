01da979df57c2e4e4f5056f9b1692bd8a926dfa2
nsd9696 <nsd26534613@gmail.com>
2025-02-15 15:21:51 -0800

    relevance, paper tool

--- tools/semantic_scholar/get-graph-get-paper/get_graph_get_paper/__main__.py
+++ tools/semantic_scholar/get-graph-get-paper/get_graph_get_paper/__main__.py
@@ -1,41 +1,36 @@
 import json
 import os
 import sys
+import requests
 
 from pydantic import BaseModel, Field
 
-token = os.getenv('AUTH_PROVIDER_TOKEN')
+token = os.getenv('SEMANTIC_SCHOLAR_TOKEN')
 
 
 class GetGraphGetPaperRequest(BaseModel):
-    param_1: str = Field(description="")
+    paper_id: int = Field(description="Semantic Scholar ID")
 
 
-def get_graph_get_paper(req: GetGraphGetPaperRequest):
-    """
-    Example of tool function
-    response = requests.delete(
-        url=f"https://www.googleapis.com/calendar/v3/calendars/{req.calendar_id}/events/{req.event_id}",
-        params={
-            "sendUpdates": req.send_updates
-        },
+def get_graph_paper(req: GetGraphGetPaperRequest):
+    response = requests.get(
+        url="https://api.semanticscholar.org/graph/v1/paper",
         headers={
             "Authorization": f"Bearer {token}",
-        }
+        },
+        params = req.model_dump(),
     )
-
+    
     if response.status_code != 200:
-        return f"failed to delete calendar events. error : {response.text}"
+        return f"failed to insert semantic scholar events. error : {response.text}"
 
-    return f"successfully deleted calendar events {req.event_id}"
-    """
-    return
+    return response.json()
 
 
 def main():
     req = json.load(sys.stdin.buffer)
     req_typed = GetGraphGetPaperRequest.model_validate(req)
-    response = get_graph_get_paper(req_typed)
+    response = get_graph_paper(req_typed)
 
     print(response)
 
--- tools/semantic_scholar/get-graph-paper-relevance-search/get_graph_paper_relevance_search/__main__.py
+++ tools/semantic_scholar/get-graph-paper-relevance-search/get_graph_paper_relevance_search/__main__.py
@@ -10,14 +10,14 @@ token = os.getenv('SEMANTIC_SCHOLAR_TOKEN')
 
 class GetGraphPaperRelevanceSearchRequest(BaseModel):
     query: str = Field(description="A plain-text search query string. No special query syntax is supported. Hyphenated query terms yield no matches (replace it with space to find matches).")
-    fields: str = Field(description="A comma-separated list of the fields to be returned. If omitted, only the paperId and title will be returned.")
-    publicationTypes: str = Field(description="Restricts results to any of the following paper publication types: Review, JournalArticle, CaseReport, ClinicalTrial, Conference, Dataset, Editorial, LettersAndComments, MetaAnalysis, News, Study, Book, BookSection.")
-    openAccessPdf: str = Field(description="Restricts results to only include papers with a public PDF. This parameter does not accept any values.")
-    minCitationCount: str = Field(description="Restricts results to only include papers with the minimum number of citations.")
-    publicationDateOrYear: str = Field(description="Restricts results to the given range of publication dates or years (inclusive). Accepts the format <startDate>:<endDate> with each date in YYYY-MM-DD format.")
-    year: str = Field(description="Restricts results to the given publication year or range of years (inclusive).")
-    venue: str = Field(description="Restricts results to papers published in the given venues, formatted as a comma-separated list.")
-    fieldsOfStudy: str = Field(description="Restricts results to papers in the given fields of study, formatted as a comma-separated list.")
+    fields: str = Field(default=None, description="A comma-separated list of the fields to be returned. If omitted, only the paperId and title will be returned.")
+    publicationTypes: str = Field(default=None, description="Restricts results to any of the following paper publication types: Review, JournalArticle, CaseReport, ClinicalTrial, Conference, Dataset, Editorial, LettersAndComments, MetaAnalysis, News, Study, Book, BookSection.")
+    openAccessPdf: str = Field(default=None, description="Restricts results to only include papers with a public PDF. This parameter does not accept any values.")
+    minCitationCount: str = Field(default=None, description="Restricts results to only include papers with the minimum number of citations.")
+    publicationDateOrYear: str = Field(default=None, description="Restricts results to the given range of publication dates or years (inclusive). Accepts the format <startDate>:<endDate> with each date in YYYY-MM-DD format.")
+    year: str = Field(default=None, description="Restricts results to the given publication year or range of years (inclusive).")
+    venue: str = Field(default=None, description="Restricts results to papers published in the given venues, formatted as a comma-separated list.")
+    fieldsOfStudy: str = Field(default=None, description="Restricts results to papers in the given fields of study, formatted as a comma-separated list.")
     offset: int = Field(default=0, description="Used for pagination. When returning a list of results, start with the element at this position in the list.")
     limit: int = Field(default=100, description="The maximum number of results to return. Must be <= 100.")
 
@@ -27,13 +27,12 @@ def get_graph_paper_relevance_search(req: GetGraphPaperRelevanceSearchRequest):
         url="https://api.semanticscholar.org/graph/v1/paper/search",
         headers={
             "Authorization": f"Bearer {token}",
-            "Notion-Version": "2022-06-28",
         },
-        json = req.model_dump(),
+        params = req.model_dump(),
     )
     
     if response.status_code != 200:
-        return f"failed to insert calendar events. error : {response.text}"
+        return f"failed to insert semantic scholar events. error : {response.text}"
 
     return response.json()
 
