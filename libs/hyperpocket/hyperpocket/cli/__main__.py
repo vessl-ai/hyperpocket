c81a250c3ed198d255b08c28d498778808edac5e
nsd9696 <nsd26534613@gmail.com>
2025-02-25 02:36:51 -0800

    Cloudflare-tool: ai-execute, create-sfu-calls-app

--- libs/hyperpocket/hyperpocket/cli/__main__.py
+++ libs/hyperpocket/hyperpocket/cli/__main__.py
@@ -2,7 +2,6 @@ import click
 
 from hyperpocket.cli.eject import eject
 from hyperpocket.cli.pull import pull
-from hyperpocket.cli.sync import sync
 from hyperpocket.cli.eject import eject
 from hyperpocket.cli.auth_token import create_token_auth_template
 from hyperpocket.cli.auth_oauth2 import create_oauth2_auth_template
@@ -28,7 +27,6 @@ devtool.add_command(build_tool)
 devtool.add_command(export_tool)
 
 cli.add_command(pull)
-cli.add_command(sync)
 cli.add_command(eject)
 
 cli()
