c20440e493df27b9477cb3c7551ef7358e38016a
Seokju Hong <seokju@vessl.ai>
2025-02-25 22:47:40 -0800

    Remove unused codes

--- examples/crewai/kraken-auto-trader/src/kraken_auto_trader/main.py
+++ examples/crewai/kraken-auto-trader/src/kraken_auto_trader/main.py
@@ -30,39 +30,3 @@ def run():
             except Exception as e:
                 raise Exception(f"An error occurred while running the crew: {e}")
 
-
-def train():
-    """
-    Train the crew for a given number of iterations.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        KrakenAutoTrader().crew().train(n_iterations=int(sys.argv[1]), filename=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while training the crew: {e}")
-
-def replay():
-    """
-    Replay the crew execution from a specific task.
-    """
-    try:
-        KrakenAutoTrader().crew().replay(task_id=sys.argv[1])
-
-    except Exception as e:
-        raise Exception(f"An error occurred while replaying the crew: {e}")
-
-def test():
-    """
-    Test the crew execution and returns the results.
-    """
-    inputs = {
-        "topic": "AI LLMs"
-    }
-    try:
-        KrakenAutoTrader().crew().test(n_iterations=int(sys.argv[1]), openai_model_name=sys.argv[2], inputs=inputs)
-
-    except Exception as e:
-        raise Exception(f"An error occurred while testing the crew: {e}")
