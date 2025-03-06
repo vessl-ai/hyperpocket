e5ec4452170c261ce80a17957238aff8766963b5
moon <moon@vessl.ai>
2025-03-06 17:32:49 +0900

    add hyperdock-wasm endpoint

--- libs/docks/hyperdock-wasm/hyperdock_wasm/__init__.py
+++ libs/docks/hyperdock-wasm/hyperdock_wasm/__init__.py
@@ -1,4 +1,5 @@
 from hyperdock_wasm.dock import WasmDock
+from hyperdock_wasm.handlers.browser.register_endpoints import register_hyperdock_wasm_endpoints
 from hyperdock_wasm.tool import WasmToolRequest
 
-__all__ = ["WasmDock", "WasmToolRequest"]
+__all__ = ["WasmDock", "WasmToolRequest", "register_hyperdock_wasm_endpoints"]
