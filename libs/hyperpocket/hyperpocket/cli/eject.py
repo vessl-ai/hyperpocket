ecaa15eb58075ff15f6786f309a7715dd3067409
moon <moon@vessl.ai>
2025-03-17 15:17:11 +0900

    remove unused command

--- libs/hyperpocket/hyperpocket/cli/eject.py
+++
@@ -1,9 +0,0 @@
-import click
-
-
-@click.command()
-@click.argument("url", type=str)
-@click.argument("ref", type=str)
-@click.argument("remote_path", type=str)
-def eject(url: str, ref: str, remote_path: str):
-    pass
--- libs/hyperpocket/hyperpocket/cli/pull.py
+++
@@ -1,8 +0,0 @@
-import click
-
-
-@click.command()
-@click.argument("url", type=str)
-@click.option("--git-ref", type=str, default="HEAD")
-def pull(url: str, git_ref: str):
-    pass
